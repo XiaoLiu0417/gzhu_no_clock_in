32119051ade0197a49c11a0770f4509e66d1bc40
XiaoLiu0417 <102782746+XiaoLiu0417@users.noreply.github.com>
2022-04-01 23:28:31 +0800

    test
    testupd

--- run_action.py
+++ run_action.py
@@ -1,9 +1,13 @@
 from login_new import login_new
 from clock_in import clock_in
 import os
+import requests
 
 stu_id = str(os.environ['STUID'])
 pwd = str(os.environ['STUPWD'])
+
+res = requests.get("https://gzhu.edu.cn")
+print(res.content)
 
 login_new(stu_id, pwd)
 clock_in(stu_id)
